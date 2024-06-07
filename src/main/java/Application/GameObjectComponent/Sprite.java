fb65105e1e6b030705c2066d552f7882735260c1
emecoding <emecoding@gmail.com>
2024-06-07 19:32:11 +0300

    Do task 19.

--- src/main/java/Application/GameObjectComponent/Sprite.java
+++ src/main/java/Application/GameObjectComponent/Sprite.java
@@ -2,14 +2,21 @@ package Application.GameObjectComponent;
 
 import Application.Application;
 import Application.ApplicationSystem.Debug;
+import Application.Resource.Shader.Shader;
 import Application.Resource.Texture;
 
+import static Application.Resource.Shader.UniformConstants.FLIP_HORIZONTALLY;
+import static Application.Resource.Shader.UniformConstants.FLIP_VERTICALLY;
 import static org.lwjgl.opengl.GL11C.GL_TEXTURE_2D;
 import static org.lwjgl.opengl.GL11C.glBindTexture;
 
 public class Sprite extends RenderComponent
 {
     private int m_Texture = -1;
+
+    private boolean m_FlipHorizontally = false;
+    private boolean m_FlipVertically = false;
+
     public Sprite(String shader_name, String texture_name)
     {
         SetShaderName(shader_name);
@@ -40,6 +47,13 @@ public class Sprite extends RenderComponent
         m_Texture = ID;
     }
 
+    public void FlipHorizontally() { m_FlipHorizontally = true; }
+    public void FlipVertically() { m_FlipVertically = true; }
+
+    public boolean IsFlippedHorizontally() { return m_FlipHorizontally; }
+    public boolean IsFlippedVertically() { return m_FlipVertically; }
+
+
     @Override
     public void Render()
     {
@@ -48,6 +62,18 @@ public class Sprite extends RenderComponent
         if(m_Texture != -1)
         {
             glBindTexture(GL_TEXTURE_2D, m_Texture);
+        }
+
+        Shader shader = Application.ResourceManager().GetShader(m_ShaderName);
+
+        if(shader.HasUniform(FLIP_HORIZONTALLY))
+        {
+            shader.SetUniform(FLIP_HORIZONTALLY, m_FlipHorizontally);
+        }
+
+        if(shader.HasUniform(FLIP_VERTICALLY))
+        {
+            shader.SetUniform(FLIP_VERTICALLY, m_FlipVertically);
         }
 
         Application.Renderer().RenderRectangle();
